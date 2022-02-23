7b95d4b2bcf29ee807d557aa0a6c89bda1d1aefc
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2022-02-23 13:30:54 +0100

    Rename Attacks to Actions

--- core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/Attacks/AbstractAttack.java
+++
@@ -1,4 +0,0 @@
-package no.ntnu.tdt4240.y2022.group23.battleshipsgame.Attacks;
-
-public abstract class AbstractAttack implements IAttack {
-}
--- core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/Attacks/NormalAttack.java
+++
@@ -1,4 +0,0 @@
-package no.ntnu.tdt4240.y2022.group23.battleshipsgame.Attacks;
-
-public class NormalAttack extends AbstractAttack {
-}
