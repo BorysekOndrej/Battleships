82f386773f8497a99feae7609a4241a53531dd7e
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2022-02-23 14:01:37 +0100

    Try how imports and declarations import Class diagram

--- core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/States/MyTurnState.java
+++ core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/States/MyTurnState.java
@@ -1,4 +1,6 @@
 package no.ntnu.tdt4240.y2022.group23.battleshipsgame.States;
+import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Actions.*;
+
 
 public class MyTurnState extends PlayTurnState {
 }
--- core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/States/PlayTurnState.java
+++ core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/States/PlayTurnState.java
@@ -1,4 +1,7 @@
 package no.ntnu.tdt4240.y2022.group23.battleshipsgame.States;
 
+import no.ntnu.tdt4240.y2022.group23.battleshipsgame.GameBoard;
+
 public class PlayTurnState extends AbstractState {
+    protected GameBoard gameBoard;
 }
