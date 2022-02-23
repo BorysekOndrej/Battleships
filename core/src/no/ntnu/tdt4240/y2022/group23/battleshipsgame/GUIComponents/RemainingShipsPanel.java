262934659ba20c60270fdb8294a59300199d61c4
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2022-02-23 20:04:09 +0100

    Move GUI components to subfolder

---
+++ core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/GUIComponents/RemainingShipsPanel.java
@@ -0,0 +1,6 @@
+package no.ntnu.tdt4240.y2022.group23.battleshipsgame.GUIComponents;
+
+import no.ntnu.tdt4240.y2022.group23.battleshipsgame.IRenderable;
+
+public class RemainingShipsPanel implements IRenderable {
+}
