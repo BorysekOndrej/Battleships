f0fbd55b90f22863d323fce237e59eafae75c708
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2022-02-23 20:04:43 +0100

    Fix ViewBoardState after moves

--- core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/States/ViewBoardState.java
+++ core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/States/ViewBoardState.java
@@ -1,13 +1,13 @@
 package no.ntnu.tdt4240.y2022.group23.battleshipsgame.States;
 
-import no.ntnu.tdt4240.y2022.group23.battleshipsgame.GameBoard;
+import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Models.GameBoard;
 import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Ships.IShip;
-import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Timer;
+import no.ntnu.tdt4240.y2022.group23.battleshipsgame.GUIComponents.TimerPanel;
 
 import java.util.List;
 
 public class ViewBoardState extends AbstractState {
     protected GameBoard gameBoard;
     protected List<IShip> ships;
-    protected Timer timer;
+    protected TimerPanel timer;
 }
