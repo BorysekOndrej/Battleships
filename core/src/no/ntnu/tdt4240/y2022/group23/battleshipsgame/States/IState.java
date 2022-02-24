958dcd30cc5a4bd99133014325868d123b0ff5f0
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2022-02-24 15:48:05 +0100

    Make IState implement IRenderable

--- core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/States/IState.java
+++ core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/States/IState.java
@@ -1,8 +1,6 @@
 package no.ntnu.tdt4240.y2022.group23.battleshipsgame.States;
 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import no.ntnu.tdt4240.y2022.group23.battleshipsgame.IRenderable;
 
-public interface IState {
-    public abstract void update(float dt);
-    public abstract void render(SpriteBatch sb);
+public interface IState extends IRenderable {
 }
