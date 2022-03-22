fa6e3ec1b0fc90d4c819d82813c81ea94ccb3646
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2022-03-22 19:42:39 +0100

    Add HTTPS client for Android

--- android/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/AndroidLauncher.java
+++ android/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/AndroidLauncher.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import no.ntnu.tdt4240.y2022.group23.battleshipsgame.BattleshipsGame;
+import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Network.httpsClient;
 
 public class AndroidLauncher extends AndroidApplication {
+	httpsClient httpsClient;
+
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
 		initialize(new BattleshipsGame(), config);
+
+		httpsClient = new httpsClient(getApplicationContext());
+		httpsClient.send("https://envojlo4sdzr8.x.pipedream.net/", null);
 	}
 }
---
+++ android/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/Network/httpsClient.java
@@ -0,0 +1,44 @@
+package no.ntnu.tdt4240.y2022.group23.battleshipsgame.Network;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.Map;
+
+public class httpsClient extends Application implements INetworkClient {
+    private static final String TAG = "MyActivity";
+    RequestQueue queue;
+
+    public httpsClient(Context ctx) {
+        queue = Volley.newRequestQueue(ctx);
+        queue.start();
+    }
+
+
+    @Override
+    public Map<String, String> receive() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean send(String url, Map<String, String> data) {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                response -> {
+                    Log.d(TAG, "Response is: " + response);
+                },
+                error -> {
+                    Log.d(TAG, "That didn't work!");
+                }
+        );
+        queue.add(stringRequest);
+
+        return true;
+    }
+}
