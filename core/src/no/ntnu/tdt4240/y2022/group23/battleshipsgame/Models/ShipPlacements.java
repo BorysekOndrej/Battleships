b4cd263d1463120458f824974b3952ad11f3672a
Anna Rechtackova <anna.rechty@gmail.com>
2022-03-23 10:44:13 +0100

    Add addShip to ShipPlacements

--- core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/Models/ShipPlacements.java
+++ core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/Models/ShipPlacements.java
@@ -13,6 +13,19 @@ import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Ships.IShip;
 public class ShipPlacements implements ISerializable {
     private List<IShip> ships;
 
+    /**
+     * Adds a ship to the collection, if it adheres to rules of ship placement:
+     * - the ship is fully within the boundaries of the game board
+     * - ship cannot overlap any already placed ship
+     * @param width width of the game board
+     * @param height height of the game board
+     * @param ship the ship to be added
+     * @throws IllegalArgumentException thrown when ship to be placed does not follow the rules
+     */
+    public void addShip(int width, int height, IShip ship) throws IllegalArgumentException {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
     public boolean hasShipOnCoords(Coords coords) {
         return getShipOnCoords(coords) != null;
     }
