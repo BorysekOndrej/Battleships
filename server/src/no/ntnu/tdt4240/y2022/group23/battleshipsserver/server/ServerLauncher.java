cf4ba6d6be5ccf5ef9d803374623e4014d0ce372
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2022-03-23 16:20:35 +0100

    Add Server project

---
+++ server/src/no/ntnu/tdt4240/y2022/group23/battleshipsserver/server/ServerLauncher.java
@@ -0,0 +1,7 @@
+package no.ntnu.tdt4240.y2022.group23.battleshipsserver.server;
+
+public class ServerLauncher {
+	public static void main (String[] arg) {
+		System.out.println("Server project started");
+	}
+}
