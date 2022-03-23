c7aff01b36499cdfbccd3b321d5e35f3bbb9db9a
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2022-03-23 11:57:05 +0100

    Implement GameBoard

--- core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/Models/GameBoard.java
+++ core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/Models/GameBoard.java
@@ -2,6 +2,8 @@ package no.ntnu.tdt4240.y2022.group23.battleshipsgame.Models;
 
 import no.ntnu.tdt4240.y2022.group23.battleshipsgame.ISerializable;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -10,8 +12,26 @@ import java.util.List;
 public class GameBoard implements ISerializable {
     private List<List<GameBoardField>> board;
 
-    public GameBoard(int width, int height) { throw new UnsupportedOperationException("not implemented"); }
-    public GameBoard(GameBoard other) { throw new UnsupportedOperationException("not implemented"); }
+    public GameBoard(int width, int height) {
+        board = new ArrayList<>();
+        for (int x = 0; x < width; x++) {
+            ArrayList<GameBoardField> newCol = new ArrayList<>(Collections.nCopies(height, GameBoardField.UNKNOWN));
+            board.add(newCol);
+        }
+    }
+    public GameBoard(GameBoard other) {
+        // we can't easily clone GameBoard, because we need deepclone
+        // we can't easily clone board, because List doesn't have clone, only ArrayList does have it
+
+        board = new ArrayList<>();
+        for (int x = 0; x < other.board.size(); x++) {
+            ArrayList<GameBoardField> newCol = new ArrayList<>();
+            for (int y = 0; y < other.board.get(0).size(); y++) {
+                newCol.add(other.get(new Coords(x, y)));
+            }
+            board.add(newCol);
+        }
+    }
 
     public GameBoardField get(Coords coords) { return board.get(coords.x).get(coords.y); }
     public void set(Coords coords, GameBoardField field) { board.get(coords.x).set(coords.y, field); }
