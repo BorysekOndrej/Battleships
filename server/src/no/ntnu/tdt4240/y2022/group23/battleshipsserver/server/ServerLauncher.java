2a9039dea130f098607172c2b2e2519352e697c5
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2022-03-24 19:45:49 +0100

    Add Firebase SDK to Server

--- server/src/no/ntnu/tdt4240/y2022/group23/battleshipsserver/server/ServerLauncher.java
+++ server/src/no/ntnu/tdt4240/y2022/group23/battleshipsserver/server/ServerLauncher.java
@@ -1,7 +1,16 @@
 package no.ntnu.tdt4240.y2022.group23.battleshipsserver.server;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.Message;
+
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.text.RandomStringGenerator;
+
+import java.io.FileInputStream;
+import java.io.IOException;
 
 import io.javalin.Javalin;
 
@@ -10,6 +19,18 @@ public class ServerLauncher {
 		System.out.println("Server project started");
 		Javalin app = Javalin.create().start(7070);
 		RedisStorage redisStorage = RedisStorage.getInstance();
+
+		try {
+			FileInputStream serviceAccount = new FileInputStream("config/firebaseSecretAccountKey.json");
+			FirebaseOptions options = FirebaseOptions.builder()
+					.setCredentials(GoogleCredentials.fromStream(serviceAccount))
+					.build();
+			FirebaseApp.initializeApp(options);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			return;
+		}
 
 		app.get("/", ctx -> ctx.result("Hello World"));
 
@@ -40,6 +61,26 @@ public class ServerLauncher {
 			}
 		});
 
+
+
+		app.post("/test_firebase_msg", ctx -> {
+			// This registration token comes from the client FCM SDKs.
+			String token = ctx.formParamAsClass("token", String.class).get();
+
+			// See documentation on defining a message payload.
+			Message message = Message.builder()
+					.putData("score", "850")
+					.putData("time", "2:45")
+					.setToken(token)
+					.build();
+
+			// Send a message to the device corresponding to the provided
+			// registration token.
+			String response = FirebaseMessaging.getInstance().send(message);
+			// Response is a message ID string.
+			System.out.println("Successfully sent message: " + response);
+
+		});
 
 	}
 }
