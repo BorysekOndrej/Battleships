430878f1b22d3fca249f121d459071f209899e2f
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2022-03-24 16:14:15 +0100

    Add user token storage

--- server/src/no/ntnu/tdt4240/y2022/group23/battleshipsserver/server/RedisStorage.java
+++ server/src/no/ntnu/tdt4240/y2022/group23/battleshipsserver/server/RedisStorage.java
@@ -1,5 +1,6 @@
 package no.ntnu.tdt4240.y2022.group23.battleshipsserver.server;
 
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 public class RedisStorage {
@@ -8,6 +9,37 @@ public class RedisStorage {
 
     private RedisStorage(){
          pool = new JedisPool("localhost", 6379);
+    }
+
+    // todo: set TTL
+
+    String getUserIDByToken(String token){
+        try (Jedis jedis = pool.getResource()) {
+            return jedis.get("user_token_" + token);
+        }
+    }
+
+    String getUserTokenByID(String userID){
+        try (Jedis jedis = pool.getResource()) {
+            return jedis.get("user_id_" + userID);
+        }
+    }
+
+    void deleteUser(String userID){
+        try (Jedis jedis = pool.getResource()) {
+            String userToken = jedis.get("user_id_" + userID);
+            jedis.del("user_id_" + userID);
+            jedis.del("user_token_" + userToken);
+        }
+    }
+
+    void setNewUserToken(String userID, String token){
+        deleteUser(userID);
+
+        try (Jedis jedis = pool.getResource()) {
+            jedis.set("user_id_" + userID, token);
+            jedis.set("user_token_" + token, userID);
+        }
     }
 
 
--- server/src/no/ntnu/tdt4240/y2022/group23/battleshipsserver/server/ServerLauncher.java
+++ server/src/no/ntnu/tdt4240/y2022/group23/battleshipsserver/server/ServerLauncher.java
@@ -1,29 +1,30 @@
 package no.ntnu.tdt4240.y2022.group23.battleshipsserver.server;
 
+import org.apache.commons.text.RandomStringGenerator;
+
 import io.javalin.Javalin;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisPool;
 
 public class ServerLauncher {
 	public static void main (String[] arg) {
 		System.out.println("Server project started");
 		Javalin app = Javalin.create().start(7070);
 		RedisStorage redisStorage = RedisStorage.getInstance();
-		JedisPool pool = redisStorage.pool;
 
 		app.get("/", ctx -> ctx.result("Hello World"));
 
 		app.post("/token", ctx -> {
-
 			String oldToken = ctx.formParam("oldToken"); // this can be null
-			String newToken = ctx.formParamAsClass("newToken", String.class).get();;
+			String newToken = ctx.formParamAsClass("newToken", String.class).get();
 
-			try (Jedis jedis = pool.getResource()) {
-				jedis.set("clientName", "Jedis");
-
-				ctx.status(200).result("Hello " + oldToken + " " + newToken + " " + jedis.get("clientName"));
+			String user_id = redisStorage.getUserIDByToken(oldToken);
+			if (user_id == null){
+				do {
+					user_id = new RandomStringGenerator.Builder().withinRange('a', 'z').build().generate(32);
+				}while (redisStorage.getUserTokenByID(user_id) != null);
 			}
-		});
+			redisStorage.setNewUserToken(user_id, newToken);
 
+			ctx.status(200).result("Hello " + oldToken + " " + newToken + " " + user_id);
+		});
 	}
 }
