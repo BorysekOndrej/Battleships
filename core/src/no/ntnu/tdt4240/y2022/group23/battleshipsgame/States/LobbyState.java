bda442bf569264dbf7b5062b3ad163c09c19f4f7
Konrad Ivelic <konrad.ivelic18@gmail.com>
2022-03-29 15:31:16 +0200

    Early state creation: Menu and Lobby state

--- core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/States/LobbyState.java
+++ core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/States/LobbyState.java
@@ -1,7 +1,34 @@
 package no.ntnu.tdt4240.y2022.group23.battleshipsgame.States;
 
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
 public class LobbyState extends AbstractState {
+    private static boolean OPPONENT = FALSE;
+
     protected LobbyState(GameStateManager gsm) {
         super(gsm);
     }
+
+    public void update(float dt){
+        if (OPPONENT == TRUE){ //Wait until opponent is found, then change to ship placement
+            goToShipPlacement();
+        }
+    }
+
+    protected void handleInput(){
+        if (Gdx.input.justTouched()){ //Place holder given change to
+            OPPONENT = TRUE;
+        }
+    };
+
+    //Changes state to ship placement state
+    private void goToShipPlacement(){
+        //Connect both players
+        gsm.set(new ShipPlacementState(gsm));
+    }
+
+    public void render(SpriteBatch sb);
+
+    public void dispose();
 }
