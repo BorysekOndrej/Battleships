0ded5e7c7f084932ec43ccd63e3663b176e64255
mikolajb21 <97767526+mikolajb21@users.noreply.github.com>
2022-04-01 15:14:19 +0200

    Added new functions implemented in Interface

--- core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/Ships/AbstractShip.java
+++ core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/Ships/AbstractShip.java
@@ -1,5 +1,6 @@
 package no.ntnu.tdt4240.y2022.group23.battleshipsgame.Ships;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Models.Coords;
@@ -9,6 +10,7 @@ import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Models.GameBoardField;
 public abstract class AbstractShip implements IShip {
     private List<Coords> positions;
     private int parts;
+    private boolean horizontal;
 
     @Override
     public List<Coords> getPositions() {
@@ -39,13 +41,47 @@ public abstract class AbstractShip implements IShip {
         this.parts = parts;
     }
 
+    public boolean getOrientation(){
+        return horizontal;
+    }
+
+    public void setOrientation(boolean horizontal){
+        this.horizontal = horizontal;
+    }
+
     @Override
     public void displace() {
-        throw new UnsupportedOperationException("not implemented");
+        int size = this.getPositions().size();
+        List<Coords> newPositions = new ArrayList<Coords>();
+        Coords start = new Coords(0,0);
+        for(int i=0;i<size;i++){
+            newPositions.add(new Coords(start.x+i, start.y));
+        }
+        this.setPositions(newPositions);
     }
 
     @Override
     public void rotateClockwise() {
-        throw new UnsupportedOperationException("not implemented");
+        int size = this.getPositions().size();
+        List<Coords> newPositions = new ArrayList<Coords>();
+        Coords start = this.getPositions().get(0);
+        Coords second = this.getPositions().get(1);
+        boolean horizontal = this.getOrientation();
+        boolean checkDirection = Math.min(second.x- start.x, second.y- start.y)<0;
+        for(int i=0;i<size;i++){
+            if(checkDirection) {
+                newPositions.add(new Coords(
+                        !horizontal ? start.x - i : start.x,
+                        !horizontal ? start.y : start.y + i
+                ));
+            }
+            else{
+                newPositions.add(new Coords(
+                        !horizontal ? start.x + i : start.x,
+                        !horizontal ? start.y : start.y - i
+                ));
+            }
+        }
+        this.setPositions(newPositions);
     }
 }
