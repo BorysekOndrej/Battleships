e9b81f26add1d8dd6d6a35406bc089069cd1f825
Anna Rechtackova <anna.rechty@gmail.com>
2022-04-06 23:36:05 +0200

    Implement server side of GameAPIClient

--- server/src/no/ntnu/tdt4240/y2022/group23/battleshipsserver/server/ServerLauncher.java
+++ server/src/no/ntnu/tdt4240/y2022/group23/battleshipsserver/server/ServerLauncher.java
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
@@ -13,11 +14,24 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Actions.IAction;
+import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Models.GameBoard;
+import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Models.GameBoardChange;
+import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Models.NextTurn;
+import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Models.ShipPlacements;
+import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Network.ServerClientMessage;
+import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Network.StringSerializer;
+import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Ships.IShip;
+import no.ntnu.tdt4240.y2022.group23.battleshipslogic.TurnEvaluator;
 
 public class ServerLauncher {
+	private static final int GAME_BOARD_WIDTH = 10;
+	private static final int GAME_BOARD_HEIGHT = 10;
+
 	private static final Logger logger = LogManager.getLogger(ServerLauncher.class);
 
 	public static void matchmaking_add(Context ctx) {
@@ -31,6 +45,105 @@ public class ServerLauncher {
 		if (twoUsersFromMatchmaking != null){
 			System.out.println(twoUsersFromMatchmaking.left + " " + twoUsersFromMatchmaking.right); // todo: implement
 		}
+	}
+
+	private static void sendMessage(Message.Builder messageBuilder, String userID) throws FirebaseMessagingException {
+		RedisStorage redisStorage = RedisStorage.getInstance();
+
+		String userToken = redisStorage.getUserTokenByID(userID);
+		Message message = messageBuilder.setToken(userToken).build();
+
+		// Send a message to the device corresponding to the provided
+		// registration token.
+		String response = FirebaseMessaging.getInstance().send(message);
+		// Response is a message ID string.
+		System.out.println("Successfully sent message: " + response);
+	}
+
+	public static void placements(Context ctx) throws FirebaseMessagingException {
+		RedisStorage redisStorage = RedisStorage.getInstance();
+
+		String userID = ctx.formParam("user_id");
+		ShipPlacements userPlacements = StringSerializer.fromString(ctx.formParam("placements"));
+
+		redisStorage.setShipPlacements(userID, userPlacements);
+
+		String opponentID = redisStorage.getOpponentId(userID);
+		ShipPlacements opponentPlacements = redisStorage.getUserShipPlacements(opponentID);
+
+		if (opponentPlacements != null) {
+			redisStorage.setUserGameBoard(userID, new GameBoard(GAME_BOARD_WIDTH, GAME_BOARD_HEIGHT));
+			redisStorage.setUserGameBoard(opponentID, new GameBoard(GAME_BOARD_WIDTH, GAME_BOARD_HEIGHT));
+
+			Message.Builder messageBuilder = Message.builder()
+					.putData("type", ServerClientMessage.GAME_START.name());
+
+			sendMessage(messageBuilder, userID);
+			// DANGER messageBuilder may have been altered by the previous call?
+			sendMessage(messageBuilder, opponentID);
+		}
+	}
+
+	public static void action(Context ctx) throws FirebaseMessagingException {
+		RedisStorage redisStorage = RedisStorage.getInstance();
+
+		String userID = ctx.formParam("user_id");
+		IAction action = StringSerializer.fromString(ctx.formParam("action"));
+
+		String opponentID = redisStorage.getOpponentId(userID);
+		GameBoard opponentBoard = redisStorage.getUserGameBoard(opponentID);
+		ShipPlacements shipPlacements = redisStorage.getUserShipPlacements(opponentID);
+
+		TurnEvaluator evaluator = new TurnEvaluator(shipPlacements, opponentBoard, action);
+
+		ServerClientMessage type = evaluator.nextTurn() == NextTurn.GAME_OVER ? ServerClientMessage.GAME_OVER : ServerClientMessage.ACTION_PERFORMED;
+		GameBoard board = evaluator.boardAfterTurn();
+		ArrayList<GameBoardChange> changedCoords = new ArrayList<>(evaluator.getChangedCoords());
+		ArrayList<IShip> unsunkShips = new ArrayList<>(shipPlacements.getUnsunkShipsDisplaced(board));
+
+		Message.Builder messageBuilder = Message.builder()
+				.putData("type", type.name())
+				.putData("board", StringSerializer.toString(board))
+				.putData("changedCoords", StringSerializer.toString(changedCoords))
+				.putData("unsunkShips", StringSerializer.toString(unsunkShips));
+
+		sendMessage(messageBuilder, userID);
+		sendMessage(messageBuilder, opponentID);
+	}
+
+	public static void timeout(Context ctx) throws FirebaseMessagingException {
+		RedisStorage redisStorage = RedisStorage.getInstance();
+
+		String userID = ctx.formParam("user_id");
+
+		String opponentID = redisStorage.getOpponentId(userID);
+		GameBoard opponentBoard = redisStorage.getUserGameBoard(opponentID);
+		ShipPlacements shipPlacements = redisStorage.getUserShipPlacements(opponentID);
+
+		ServerClientMessage type = ServerClientMessage.ACTION_PERFORMED;
+		ArrayList<GameBoardChange> changedCoords = new ArrayList<>();
+		ArrayList<IShip> unsunkShips = new ArrayList<>(shipPlacements.getUnsunkShipsDisplaced(opponentBoard));
+
+		Message.Builder messageBuilder = Message.builder()
+				.putData("type", type.name())
+				.putData("board", StringSerializer.toString(opponentBoard))
+				.putData("changedCoords", StringSerializer.toString(changedCoords))
+				.putData("unsunkShips", StringSerializer.toString(unsunkShips));
+
+		sendMessage(messageBuilder, userID);
+		sendMessage(messageBuilder, opponentID);
+	}
+
+	public static void terminate(Context ctx) throws FirebaseMessagingException {
+		RedisStorage redisStorage = RedisStorage.getInstance();
+
+		String userID = ctx.formParam("user_id");
+		String opponentID = redisStorage.getOpponentId(userID);
+
+		Message.Builder messageBuilder = Message.builder()
+				.putData("type", ServerClientMessage.OTHER_ENDED_COMMUNICATION.name());
+
+		sendMessage(messageBuilder, opponentID);
 	}
 
 	public static void main (String[] arg) {
@@ -61,6 +174,10 @@ public class ServerLauncher {
 		});
 
 		app.post("/matchmaking_add", ServerLauncher::matchmaking_add);
+		app.post("/placements", ServerLauncher::placements);
+		app.post("/action", ServerLauncher::action);
+		app.post("/timeout", ServerLauncher::timeout);
+		app.post("/terminate", ServerLauncher::terminate);
 
 		app.post("/test_firebase_msg", ctx -> {
 			// This registration token comes from the client FCM SDKs.
