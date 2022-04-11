a89dce92a3c9be619d8f7ecc6647bcbeabeaede7
adamrog <adamrog@stud.ntnu.no>
2022-04-11 22:26:08 +0200

    graphics added; MainMenu update; GameBoardPanel basis added

--- core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/GUIComponents/GameBoardPanel.java
+++ core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/GUIComponents/GameBoardPanel.java
@@ -1,30 +1,132 @@
 package no.ntnu.tdt4240.y2022.group23.battleshipsgame.GUIComponents;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import java.awt.Rectangle;
+
 import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Models.GameBoard;
+import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Models.Coords;
+import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Models.GameBoardField;
 import no.ntnu.tdt4240.y2022.group23.battleshipsgame.IRenderable;
 
 public class GameBoardPanel implements IRenderable {
+    private static final int FIELD_SIZE = 86;
+    private static final int GAMEBORAD_OFFSET = 2;
+    private static final int GAMEBORAD_ROWS = 10;
+
+
     private GameBoard gameBoard;
 
-    @Override
-    public void handleInput() {
-        throw new UnsupportedOperationException("not implemented");
+    private Texture gameBoardTex;
+
+    private Texture unknownField;
+    private Texture waterField;
+    private Texture hitField;
+    private Texture sunkField;
+    private Texture shipField;
+
+    private int xCord;
+    private int yCord;
+
+    private Rectangle bonds;
+
+    public GameBoardPanel(int x, int y){
+        gameBoardTex = new Texture("game_board/gameboard.png");
+        unknownField = new Texture("game_board/unknown.png");
+        waterField = new Texture("game_board/water.png");
+        hitField = new Texture("game_board/hit.png");
+        sunkField = new Texture("game_board/sunk.png");
+        shipField = new Texture("game_board/ship.png");
+
+        xCord = x;
+        yCord = y;
+
+        bonds = new Rectangle(xCord, yCord, gameBoardTex.getWidth(), gameBoardTex.getHeight());
+
+        gameBoard = new GameBoard(GAMEBORAD_ROWS, GAMEBORAD_ROWS);
+        gameBoard.set(new Coords(0,0), GameBoardField.SUNK);
+        gameBoard.set(new Coords(1,0), GameBoardField.SUNK);
+        gameBoard.set(new Coords(2,0), GameBoardField.SUNK);
+        gameBoard.set(new Coords(3,0), GameBoardField.WATER);
+        gameBoard.set(new Coords(4,0), GameBoardField.WATER);
+        gameBoard.set(new Coords(5,0), GameBoardField.HIT);
+        gameBoard.set(new Coords(0,1), GameBoardField.WATER);
+        gameBoard.set(new Coords(4,4), GameBoardField.WATER);
     }
 
-    @Override
-    public void update(float dt) {
-        throw new UnsupportedOperationException("not implemented");
+    //Args: int x: Gdx.input.getX();
+    //      int y: Gdx.input.getY();
+    public Coords getFieldCoords(int x, int y){
+        int xRelative = x - xCord;
+        int yRelative = y - (1920 - yCord - gameBoardTex.getHeight());
+        Coords result = new Coords((int) ((xRelative - GAMEBORAD_OFFSET) / FIELD_SIZE - 1), (int) -((yRelative - GAMEBORAD_OFFSET) / FIELD_SIZE - 1));
+        if(coordsValid(result)) return result;
+        else  {
+            System.out.println("Coords out of bonds");
+            return null;
+        }
     }
 
-    @Override
-    public void render(SpriteBatch sb) {
-        throw new UnsupportedOperationException("not implemented");
+    public boolean coordsValid(Coords coords){
+        return (coords.x >= 0 && coords.x < GAMEBORAD_ROWS) && (coords.y >= 0 && coords.y < GAMEBORAD_ROWS);
     }
 
-    @Override
-    public void dispose() {
-        throw new UnsupportedOperationException("not implemented");
+
+    public void drawField(GameBoardField field, SpriteBatch sb, int xCord, int yCord){
+        switch (field) {
+            case UNKNOWN:
+                sb.draw(unknownField, xCord, yCord);
+                break;
+            case WATER:
+                sb.draw(waterField, xCord, yCord);
+                break;
+            case HIT:
+                sb.draw(hitField, xCord, yCord);
+                break;
+            case SUNK:
+                sb.draw(sunkField, xCord, yCord);
+                break;
+            case SHIP:
+                sb.draw(shipField, xCord, yCord);
+                break;
+            default:
+        }
+    }
+
+    public void handleInput(){
+        if(Gdx.input.justTouched() /*&& bonds.contains(Gdx.input.getX() - xCord, Gdx.input.getY() - yCord)*/){
+            int x = Gdx.input.getX();
+            int y = Gdx.input.getY();
+            Coords res = getFieldCoords(x, y);
+            if (res != null) System.out.println(res.toString());
+        }
+    }
+    public void update(float dt){
+        handleInput();
+    }
+    public void render(SpriteBatch sb){
+        sb.begin();
+        sb.draw(gameBoardTex, xCord, yCord);
+
+        for(int i = 0; i < GAMEBORAD_ROWS; i++){
+            for(int j = 0; j < GAMEBORAD_ROWS; j++){
+                GameBoardField actualField = gameBoard.get(new Coords(i, j));
+                int xLocus = xCord + GAMEBORAD_OFFSET + FIELD_SIZE * (i + 1);
+                int yLocus = yCord + GAMEBORAD_OFFSET + FIELD_SIZE * (GAMEBORAD_ROWS - 1 - j);
+                drawField(actualField, sb, xLocus, yLocus);
+            }
+        }
+        sb.end();
+    }
+
+    public void dispose(){
+        gameBoardTex.dispose();
+        unknownField.dispose();
+        waterField.dispose();
+        hitField.dispose();
+        sunkField.dispose();
+        shipField.dispose();
     }
 }
--- core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/GUIComponents/MainMenu.java
+++ core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/GUIComponents/MainMenu.java
@@ -1,24 +1,73 @@
 package no.ntnu.tdt4240.y2022.group23.battleshipsgame.GUIComponents;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.graphics.Texture;
 
 import no.ntnu.tdt4240.y2022.group23.battleshipsgame.IRenderable;
 
 public class MainMenu implements IRenderable {
-    @Override
-    public void handleInput() { throw new UnsupportedOperationException("not implemented"); }
+    final static public int HEIGHT = 1920;
+    final static public int WIDTH = 1080;
+
+    private final Texture background;
+    private final Texture createLobbyTex;
+    private SimpleButton createLobbyButton;
+
+    private final Texture joinLobbyTex;
+    private SimpleButton joinLobbyButton;
+
+    private final Texture leaderBoardTex;
+    private SimpleButton leaderBoardButton;
+
+    public MainMenu(){
+        background = new Texture("main_menu/game_menu.png");
+
+        createLobbyTex = new Texture("main_menu/create_lobby.png");
+        createLobbyButton = new SimpleButton(WIDTH/2 - createLobbyTex.getWidth()/2, HEIGHT/2 - createLobbyTex.getHeight()/2, createLobbyTex);
+
+        joinLobbyTex = new Texture("main_menu/join_lobby.png");
+        joinLobbyButton = new SimpleButton(WIDTH/2 - joinLobbyTex.getWidth()/2, HEIGHT/2 - joinLobbyTex.getHeight()*3/2, joinLobbyTex);
+
+        leaderBoardTex = new Texture("main_menu/leaderboard.png");
+        leaderBoardButton = new SimpleButton(WIDTH/2 - leaderBoardTex.getWidth()/2, HEIGHT/2 - leaderBoardTex.getHeight()*5/2, leaderBoardTex);
+
+    }
+    public boolean createLobbyButtonPressed(){
+        return createLobbyButton.buttonTouched();
+    }
+
+    public boolean joinLobbyButtonPressed(){
+        return joinLobbyButton.buttonTouched();
+    }
+
+    public boolean leaderBoardButtonPressed(){
+        return leaderBoardButton.buttonTouched();
+    }
 
     @Override
-    public void update(float dt) { throw new UnsupportedOperationException("not implemented");}
+    public void handleInput() {}
 
     @Override
-    public void render(SpriteBatch sb) { throw new UnsupportedOperationException("not implemented");}
+    public void update(float dt) {
+        handleInput();
+    }
 
     @Override
-    public void dispose() { throw new UnsupportedOperationException("not implemented");}
+    public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(background, 0,0, WIDTH, HEIGHT);
+        sb.draw(createLobbyButton.getTexture(), createLobbyButton.getPosX(), createLobbyButton.getPosY());
+        sb.draw(joinLobbyButton.getTexture(), joinLobbyButton.getPosX(), joinLobbyButton.getPosY());
+        sb.draw(leaderBoardButton.getTexture(), leaderBoardButton.getPosX(), leaderBoardButton.getPosY());
+        sb.end();
+    }
 
-    public boolean createLobbyButtonPressed(){return false;}
-
-    public boolean joinLobbyButtonPressed(){return false;}
+    @Override
+    public void dispose() {
+        background.dispose();
+        joinLobbyTex.dispose();
+        createLobbyTex.dispose();
+        leaderBoardTex.dispose();
+    }
 }
+
