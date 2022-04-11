9f3382ae76b59de3a927fe4de35f4ce63e1ded04
adamrog <adamrog@stud.ntnu.no>
2022-04-11 23:08:37 +0200

    Fonts added; timerPanel basis added

--- core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/GUIComponents/TimerPanel.java
+++ core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/GUIComponents/TimerPanel.java
@@ -1,12 +1,32 @@
 package no.ntnu.tdt4240.y2022.group23.battleshipsgame.GUIComponents;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
 import no.ntnu.tdt4240.y2022.group23.battleshipsgame.IRenderable;
 import no.ntnu.tdt4240.y2022.group23.battleshipsgame.Models.Timer;
 
 public class TimerPanel implements IRenderable {
     private Timer timer;
+    private FreeTypeFontGenerator generator;
+    private BitmapFont font;
+    private int xCord;
+    private int yCord;
+
+    public TimerPanel(int x, int y){
+        xCord = x;
+        yCord = y;
+        timer = new Timer();
+        generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/Arcon-Regular.otf"));
+        FreeTypeFontGenerator.FreeTypeFontParameter params = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        params.size = 24;
+        params.color = Color.BLACK;
+        font = generator.generateFont(params);
+    }
+
 
     public void place(float x, float y, float width, float height) {
         throw new UnsupportedOperationException("not implemented");
@@ -28,12 +48,14 @@ public class TimerPanel implements IRenderable {
 
     @Override
     public void render(SpriteBatch sb) {
-        throw new UnsupportedOperationException("not implemented");
+        sb.begin();
+        font.draw(sb, "Test", xCord, yCord);
+        sb.end();
     }
 
     @Override
     public void dispose() {
-        throw new UnsupportedOperationException("not implemented");
+        font.dispose();
     }
 
     public boolean runOut() {throw new UnsupportedOperationException("not implemented");}
