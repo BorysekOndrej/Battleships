60cae3b5819ed7f06265d014eb1700839ff4e989
adamrog <adamrog@stud.ntnu.no>
2022-04-15 18:47:20 +0200

    graphisc added
    action buttons and finish game munus

--- core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/GUIComponents/ActionPanel.java
+++ core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/GUIComponents/ActionPanel.java
@@ -11,10 +11,12 @@ public class ActionPanel implements IRenderable {
     private Texture  panel;
     private ActionButton singleShot;
     private ActionButton radar;
+    private ActionButton nuke;
+    private ActionButton surr;
     private final Texture buttonMarked;
-    private final Texture unknown;
+    //private final Texture unknown;
 
-    private boolean markedActions[] = {false, false};
+    private boolean markedActions[] = {false, false, false, false};
 
     int xPos;
     int yPos;
@@ -25,13 +27,17 @@ public class ActionPanel implements IRenderable {
         panel = new Texture("action_panel/action_panel.png");
         singleShot = new ActionButton(new Texture("action_panel/attack_button.png"), xPos + 10 + 26, yPos + 10 +10);
         radar = new ActionButton(new Texture("action_panel/radar_button.png"), xPos + 10 + 26 * 2 + 125, yPos + 10 +10);
+        nuke = new ActionButton(new Texture("action_panel/nuke_button.png"), xPos + 10 + 26 * 3 + 125 * 2, yPos + 10 +10);
+        surr = new ActionButton(new Texture("action_panel/surr_button.png"), xPos + 10 + 26 * 4 + 125 * 3, yPos + 10 +10);
         buttonMarked = new Texture("action_panel/button_marked.png");
-        unknown = new Texture("action_panel/unknown_button.png");
+        //unknown = new Texture("action_panel/unknown_button.png");
     }
 
     private void resetMarkedActions(){
         markedActions[0] = false;
         markedActions[1] = false;
+        markedActions[2] = false;
+        markedActions[3] = false;
     }
 
     private void markAction(int type){
@@ -47,11 +53,18 @@ public class ActionPanel implements IRenderable {
     public void handleInput() {
         if(singleShot.buttonTouched()){
             markAction(0);
-            singleShot.setActive(false);
+            //singleShot.setActive(false);
         }
         if (radar.buttonTouched()) {
             markAction(1);
-            radar.setActive(false);
+            //radar.setActive(false);
+        }
+        if(nuke.buttonTouched()){
+            markAction(2);
+        }
+        if (surr.buttonTouched()) {
+            markAction(3);
+            surr.setActive(false);
         }
     }
 
@@ -65,11 +78,15 @@ public class ActionPanel implements IRenderable {
         sb.draw(panel, xPos, yPos);
         singleShot.render(sb);
         radar.render(sb);
-        sb.draw(unknown, xPos + 10 + 26 * 3 + 125 * 2, yPos + 10 +10);
-        sb.draw(unknown, xPos + 10 + 26 * 4 + 125 * 3, yPos + 10 +10);
+        nuke.render(sb);
+        surr.render(sb);
+        //sb.draw(unknown, xPos + 10 + 26 * 3 + 125 * 2, yPos + 10 +10);
+        //sb.draw(unknown, xPos + 10 + 26 * 4 + 125 * 3, yPos + 10 +10);
 
         if (markedActions[0]) sb.draw(buttonMarked, singleShot.getXPos(), singleShot.getYPos());
         if (markedActions[1]) sb.draw(buttonMarked, radar.getXPos(), radar.getYPos());
+        if (markedActions[2]) sb.draw(buttonMarked, nuke.getXPos(), nuke.getYPos());
+        if (markedActions[3]) sb.draw(buttonMarked, surr.getXPos(), surr.getYPos());
     }
 
     @Override
