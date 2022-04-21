5984f1d2676b9b79cd2080615a7a0c5fe0dbc561
Ondřej Borýsek <BorysekOndrej@users.noreply.github.com>
2022-04-21 15:28:33 +0200

    Fix Casual matchmaking

--- core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/States/MatchmakingLobbyState.java
+++ core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/States/MatchmakingLobbyState.java
@@ -11,7 +11,7 @@ public class MatchmakingLobbyState extends AbstractLobbyState{
     protected MatchmakingLobbyState(GameStateManager gsm) {
         super(gsm);
         randOppLobbyStateGUI = new RandOppLobbyStateGUI();
-        lobbyAPIClient.sendJoinMatchmakingRequest();
+        lobbyAPIClient.sendJoinCasualMatchmakingRequest();
     }
 
     @Override
