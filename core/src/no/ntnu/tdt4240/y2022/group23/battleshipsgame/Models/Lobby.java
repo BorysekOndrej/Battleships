fbce75aa5028ebc1009d2940fbfcc10f754f0178
Anna Rechtackova <anna.rechty@gmail.com>
2022-04-22 17:52:33 +0200

    Remove unused classes

--- core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/Models/Lobby.java
+++
@@ -1,7 +0,0 @@
-package no.ntnu.tdt4240.y2022.group23.battleshipsgame.Models;
-
-import java.io.Serializable;
-
-public class Lobby implements Serializable {
-    private String id;
-}
--- core/src/no/ntnu/tdt4240/y2022/group23/battleshipsgame/Models/Timer.java
+++
@@ -1,7 +0,0 @@
-package no.ntnu.tdt4240.y2022.group23.battleshipsgame.Models;
-
-import java.io.Serializable;
-
-public class Timer implements Serializable {
-    long endTimestamp;
-}
